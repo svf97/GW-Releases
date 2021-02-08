353b95d53cb5de8b8f7522a42069a87539d77e78
samarthdd <samarth@smalldaytech.com>
2021-02-09 03:24:31 +0530

    updated script for visualization

--- scripts/submodule_visualization.py
+++ scripts/submodule_visualization.py
@@ -3,9 +3,11 @@ import configparser
 import subprocess
 import pydot
 import click
-level=0
+import requests
 
-#RUN : python ./scripts/submodule_visualization.py -m png ../GW-Releases
+
+
+#RUN : python ./scripts/submodule_visualization.py ../GW-Releases
 
 class Tree(object):
 
@@ -16,6 +18,7 @@ class Tree(object):
 
     def createChild(self,tree):
         self.children.append(tree)
+
     def getChildren(self):
         return self.children
 
@@ -31,70 +34,92 @@ class Tree(object):
     def getData(self):
         return self.data
 
-    def print_submodule_info(self, indentation=0, with_url=True):
-        label = self._getLabel(with_url)
-        indent = indentation * '---'
-        # Add spacing when we actually indent
-        indent += ' ' if indent else ''
-        print(indent + label)
-        for child in self.children:
-            child.print_submodule_info(indentation + 1, with_url)
+    def get_main_repo_name(self):
+        main_repo_cmd=["git","rev-parse","--show-toplevel"]
+        parent_repo_name=subprocess.check_output(main_repo_cmd, encoding='UTF-8').split("/")[-1].strip()
+        return parent_repo_name
 
-    def buildGraph(self, graph, parent, indentation, graphmode, with_url):
-        global level
+    def get_master_tag(self):
+        get_tag_commit = ["git", "rev-list", "--tags", "--max-count=1"]
+        tags=['git', 'fetch', '--all', '--tags']
+        subprocess.check_output(tags)
+        tag_commit_id = subprocess.check_output(get_tag_commit, encoding='UTF-8').strip()
+        args = ["git", "describe", "--tags", tag_commit_id]
+        master_tag = subprocess.check_output(args, encoding='UTF-8')
+        return master_tag
+
+    def buildGraph(self, graph, parent, indentation, graphmode, with_url,level=0):
         color="#ACE7EF"
+
         if level==0:
             color="#FFBEBC"
         elif level==1:
             color="#FFF5BA"
-        label = self._getLabel(with_url, "\n")
+
+        label = self.get_Label(with_url, "\n")
+
         # Add explicit quotation marks to avoid parsing confusion in dot
         if graphmode == 'scattered':
             node = pydot.Node('"' + label + '"',style="filled", fillcolor=color,shape='box', fontsize='22')
         else:
             node = pydot.Node('"' + label + '"', fillcolor=color,shape='box', fontsize='22')
         indentation += 20
+
         if parent is not None:
             graph.add_edge(pydot.Edge(parent, node))
         graph.add_node(node)
+
         if self.children:
             level=level+1
+
         for child in self.children:
-
             [graph, indentation] = child.buildGraph(
-                graph, node, indentation, graphmode, with_url)
+                graph, node, indentation, graphmode, with_url,level)
             indentation += 1
-
-        if self.children:
-            level=level-1
-
         return [graph, indentation]
 
-    def _getLabel(self, with_url, sep=' - '):
+    def get_parent_repo(self,repo_name):
+        parent_repo=None
+        api_url = "https://api.github.com/repos/%s" % repo_name
+        details = requests.get(api_url)
+        if details.status_code is 200:
+            json = details.json()
+            if "parent" in json:
+                parent_repo = json["parent"]["full_name"]
 
-        label = self.data['name']
-        label = label + "-"
+        return parent_repo
 
-        json=self.get_submodules_json()
 
+    def get_Label(self, with_url, sep=' - '):
+        label = ""
+        main_repo_name=self.get_main_repo_name()
+        if main_repo_name in self.data['name']:
+            label=sep + label+self.data["name"]
+            label = label + sep+self.get_master_tag()+sep
+
+        if with_url and 'url' in self.data and self.data['url']:
+            repo_name = self.data['url'].replace("https://github.com/", "")
+            label += sep + repo_name
+            parent_repo=self.get_parent_repo(repo_name.replace(".git",""))
+            if parent_repo:
+                if parent_repo:
+                    label += sep + "(Forked from: " + parent_repo + ")"
+                    print(parent_repo)
+
+        json = self.get_submodules_json()
         for each in json:
             each = each.split()
             repo_name=each[1].decode("utf-8")
 
             if repo_name.split("/")[-1]==self.data['name']:
                 commit_id=each[0].decode("utf-8")
-                tag=each[2].decode("utf-8")
-                label +=  f"tag_id={tag}"
-                label = label + "\n"
-                #label += sep +f"commit_id={commit_id}"
-
-        if with_url and 'url' in self.data and self.data['url']:
-            label += sep + self.data['url'].replace("https://github.com/", "")
+                tag=each[2].decode("utf-8").strip("()")
+                label +=  sep+f"{tag}"+sep
+                label=label+sep
 
         return label
 
     def get_submodules_json(self):
-        submodules_list=[]
         submodules_list = subprocess.check_output(["git", "submodule", "status", "--recursive"],
                                                   stderr=subprocess.STDOUT, )
         submodules_list = submodules_list.splitlines()
@@ -105,41 +130,35 @@ class Tree(object):
             repo_url = submodules_json[2]
         return submodules_list
 
+class Parser:
+    def parseGitModuleFile(self,file):
+        config = configparser.ConfigParser()
+        config.read(file)
+        res = []
+        for section in config.sections():
+            p = os.path.join(config[section]['path'])
+            u = config[section]['url']
+            res.append((p, u))
+        return res
 
-def parseGitModuleFile(file):
-    config = configparser.ConfigParser()
-    config.read(file)
-    res = []
-    for section in config.sections():
-        p = os.path.join(config[section]['path'])
-        u = config[section]['url']
-        res.append((p, u))
-    return res
+    def parse(self,path, url=None):
+        if os.path.isfile(os.path.join(path, '.gitmodules')) is False:
+            return Tree({'name': os.path.basename(os.path.normpath(path)),
+                         'path': path, 'url': url})
 
-def parse(path, url=None):
-    if os.path.isfile(os.path.join(path, '.gitmodules')) is False:
-        return Tree({'name': os.path.basename(os.path.normpath(path)),
+        tree = Tree({'name': os.path.basename(os.path.normpath(path)),
                      'path': path, 'url': url})
+        moduleFile = os.path.join(path, '.gitmodules')
 
-
-
-    tree = Tree({'name': os.path.basename(os.path.normpath(path)),
-                 'path': path, 'url': url})
-    moduleFile = os.path.join(path, '.gitmodules')
-
-    if os.path.isfile(moduleFile) is True:
-        subs = parseGitModuleFile(moduleFile)
-        for p, u in subs:
-            newPath = os.path.join(path, p)
-            newTree = parse(newPath, u)
-            tree.createChild(newTree)
-    return tree
+        if os.path.isfile(moduleFile) is True:
+            subs = self.parseGitModuleFile(moduleFile)
+            for p, u in subs:
+                newPath = os.path.join(path, p)
+                newTree = self.parse(newPath, u)
+                tree.createChild(newTree)
+        return tree
 
 @click.command()
-@click.option('-m', '--mode',
-              default='text',
-              show_default=True,
-              help="Output Mode: text | png")
 @click.option('-g', '--graphmode',
               default='scattered',
               show_default=True,
@@ -148,26 +167,20 @@ def parse(path, url=None):
               default='graph',
               show_default=True,
               help="Image filename")
-@click.option('-u', '--with-url',
-              default=False, is_flag=True,
-              show_default=True,
-              help="Add repo URLs")
+
 @click.argument('repo')
-def main(mode, repo, graphmode, out, with_url=True):
 
+def main(repo, graphmode, out):
     root = repo
-    tree = parse(root)
-
-    if mode == 'text':
-        tree.print_submodule_info(with_url=with_url)
-    else:
-        graph = pydot.Dot(graph_type='digraph',rankdir = 'LR')
-        [graph, indentation] = tree.buildGraph(graph, None, 1, graphmode, with_url=True)
-        filename = out + '.png'
-        graph.write_png(filename)
-
+    parser=Parser()
+    tree = parser.parse(root)
+    graph = pydot.Dot(graph_type='digraph',rankdir = 'LR')
+    [graph, indentation] = tree.buildGraph(graph, None, 1, graphmode, with_url=True)
+    filename = out + '.png'
+    graph.write_png(filename)
 
 if __name__ == '__main__':
     main()
+
 
 
